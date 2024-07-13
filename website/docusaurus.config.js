9953caab9fc9762fae030ece124e937513b06ed9
GengYiran <2000012976@stu.pku.edu.cn>
2024-07-14 02:19:19 +0800

    update github url

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -80,7 +80,7 @@ const config = {
           },
           {to: '/blog', label: 'Blog', position: 'left'},
           {
-            href: 'https://github.com/facebook/docusaurus',
+            href: 'https://github.com/Genesis-Embodied-AI',
             label: 'GitHub',
             position: 'right',
           },
